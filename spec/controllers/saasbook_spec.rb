5befae31e55107b33723277dc3978f0acbc5608d
Joshua Delrosario <joshua.d.delrosario@gmail.com>
2022-04-19 20:29:51 -0700

    one more test

--- spec/controllers/saasbook_spec.rb
+++ spec/controllers/saasbook_spec.rb
@@ -46,6 +46,32 @@ RSpec.describe SaasbookController, type: :controller do
         expect(@num_annotations_final).to eq(@num_annotations_after)
       end
     end
+    describe 'creating annotations on different pages' do
+      login_user
+      it 'should result in multiple annotations being present in the database' do
+        @user = User.find_by(uid: 43_231, provider: 'github')
+        @annotation_find = @user.page_annotations.find_by(chapter: '1', section: '4')
+        expect(@annotation_find).to be_nil
+        @num_annotations_before = PageAnnotation.count
+
+        post :annotate, params: { chapter: '1', section: '4', annotation: 'hello world' }
+
+        @annotation_find = @user.page_annotations.find_by(chapter: '1', section: '4')
+        expect(@annotation_find.annotation).to eq('hello world')
+        @num_annotations_after = PageAnnotation.count
+        expect(@num_annotations_after).to eq(@num_annotations_before + 1)
+
+        post :annotate, params: { chapter: '5', section: '5', annotation: 'goodbye moon' }
+
+        @annotation_find = @user.page_annotations.find_by(chapter: '1', section: '4')
+        expect(@annotation_find.annotation).to eq('hello world')
+        @annotation_find = @user.page_annotations.find_by(chapter: '5', section: '5')
+        expect(@annotation_find.annotation).to eq('goodbye moon')
+        @num_annotations_final = PageAnnotation.count
+        expect(@num_annotations_final).to eq(@num_annotations_after + 1)
+        expect(@num_annotations_final).to eq(@num_annotations_before + 2)
+      end
+    end
     describe 'making an annotation without logging in' do
       it 'should fail spectacularly' do
         @num_annotations_before = PageAnnotation.count
